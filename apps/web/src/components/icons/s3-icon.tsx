82d1931e7ed9599911c59ad521847fd1e663d247
Sashank Bhamidi <hello@sashank.wiki>
2025-07-25 10:56:35 +0530

    Add S3 icon component
    Creates professional S3 bucket icon using the official AWS S3 visual style:
    - SVG-based implementation with customizable size and color
    - Consistent with existing icon component patterns
    - Used in S3 provider selection interface

---
+++ apps/web/src/components/icons/s3-icon.tsx
@@ -0,0 +1,10 @@
+export default function S3Icon() {
+	return (
+		<svg viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
+			{/* S3 bucket icon with cloud/storage theme */}
+			<path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L5.59 10.42c-.39.39-.39 1.02 0 1.41L11.3 17.54c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 11H19c.55 0 1-.45 1-1s-.45-1-1-1z" />
+			<path d="M2 5v14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2zm2 0h16v14H4V5z" />
+			<path d="M6 7h12v2H6zm0 4h8v2H6zm0 4h10v2H6z" />
+		</svg>
+	);
+}
