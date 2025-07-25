64164a82d6ef829a4b68a5e40d823dca9f373df1
Sashank Bhamidi <hello@sashank.wiki>
2025-07-25 12:26:26 +0530

    Improve S3 icon design to better represent cloud storage
    Replace arrow icon with bucket/cloud storage design that visually communicates S3 functionality

--- apps/web/src/components/icons/s3-icon.tsx
+++ apps/web/src/components/icons/s3-icon.tsx
@@ -2,10 +2,9 @@ export default function S3Icon() {
 	return (
 		<svg viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5" role="img" aria-label="Amazon S3 storage">
 			<title>Amazon S3 storage</title>
-			{/* S3 bucket icon with cloud/storage theme */}
-			<path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L5.59 10.42c-.39.39-.39 1.02 0 1.41L11.3 17.54c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 11H19c.55 0 1-.45 1-1s-.45-1-1-1z" />
-			<path d="M2 5v14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2zm2 0h16v14H4V5z" />
-			<path d="M6 7h12v2H6zm0 4h8v2H6zm0 4h10v2H6z" />
+			{/* S3 bucket icon representing cloud storage */}
+			<path d="M12 2L2 7v10c0 5.55 3.84 9.739 9 11 5.16-1.261 9-5.45 9-11V7l-10-5z" />
+			<path d="M7 10h10v2H7zm0 3h8v2H7zm0 3h6v2H7z" />
 		</svg>
 	);
 }
