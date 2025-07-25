b70393317916499dc8e1a26e48d8505b53a16278
Sashank Bhamidi <hello@sashank.wiki>
2025-07-25 12:06:06 +0530

    Add accessibility attributes to S3 icon for screen readers

--- apps/web/src/components/icons/s3-icon.tsx
+++ apps/web/src/components/icons/s3-icon.tsx
@@ -1,6 +1,7 @@
 export default function S3Icon() {
 	return (
-		<svg viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
+		<svg viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5" role="img" aria-label="Amazon S3 storage">
+			<title>Amazon S3 storage</title>
 			{/* S3 bucket icon with cloud/storage theme */}
 			<path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L5.59 10.42c-.39.39-.39 1.02 0 1.41L11.3 17.54c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 11H19c.55 0 1-.45 1-1s-.45-1-1-1z" />
 			<path d="M2 5v14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2zm2 0h16v14H4V5z" />
