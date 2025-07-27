3f23a19f649b50b74cc3bd3482f4adbdccc3dd09
Gaurav Nardia <gauravnardia07@gmail.com>
2025-07-27 07:54:15 +0530

    chore: fix formatting issues for prettier check

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,23 +1,20 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  transform: {
-    '^.+\\.[tj]sx?$':['ts-jest', { useESM: true }],
-  },
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(jose)/)',
-    
-  ],
-  testMatch: ['**/tests/**/*.test.ts'],
-  forceExit: true,
+	preset: "ts-jest/presets/default-esm",
+	testEnvironment: "node",
+	extensionsToTreatAsEsm: [".ts", ".tsx"],
+	transform: {
+		"^.+\\.[tj]sx?$": ["ts-jest", { useESM: true }],
+	},
+	globals: {
+		"ts-jest": {
+			useESM: true,
+		},
+	},
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1",
+	},
+	transformIgnorePatterns: ["/node_modules/(?!(jose)/)"],
+	testMatch: ["**/tests/**/*.test.ts"],
+	forceExit: true,
 };
