b2b63385533e635fcbaffc835e424ac49f500607
Sashank Bhamidi <hello@sashank.wiki>
2025-08-03 10:27:26 +0530

    feat: add Box cloud storage icon component

---
+++ apps/web/src/components/icons/box-icon.tsx
@@ -0,0 +1,10 @@
+export default function BoxIcon() {
+	return (
+		<svg viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5" role="img" aria-label="Box cloud storage">
+			<title>Box cloud storage</title>
+			{/* Box logo design */}
+			<path d="M3 3h18v18H3V3zm2 2v14h14V5H5z" />
+			<path d="M8 8h8v2H8zm0 3h6v2H8zm0 3h4v2H8z" />
+		</svg>
+	);
+}
