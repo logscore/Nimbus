b42e74297b2dcb8cdef0ef891a156850ec693151
Sashank Bhamidi <hello@sashank.wiki>
2025-08-03 10:50:52 +0530

    fix: update Box icon to use official Box branding colors

--- apps/web/src/components/icons/box-icon.tsx
+++ apps/web/src/components/icons/box-icon.tsx
@@ -1,9 +1,10 @@
 export default function BoxIcon() {
 	return (
-		<svg viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5" role="img" aria-label="Box cloud storage">
-			<title>Box cloud storage</title>
-			{/* Generic cloud storage icon */}
-			<path d="M19.35 10.04A7.49 7.49 0 0 0 12 4C9.11 4 6.6 5.64 5.35 8.04A5.994 5.994 0 0 0 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96z" />
+		<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" role="img">
+			<title>Box</title>
+			{/* Official Box branding colors and styling */}
+			<rect x="2" y="2" width="20" height="20" rx="2" fill="#0075C9" />
+			<path d="M6 8h12v1.5H6V8zm0 3h10v1.5H6V11zm0 3h8v1.5H6V14z" fill="white" />
 		</svg>
 	);
 }
