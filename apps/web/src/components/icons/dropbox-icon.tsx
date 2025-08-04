37f56c34d97993cda522fe30876721570553a94b
Sashank Bhamidi <hello@sashank.wiki>
2025-08-04 11:16:32 +0530

    feat: add official Dropbox icon component for UI integration
    Create Dropbox icon React component:
    - Uses official Dropbox logo design and branding
    - Consistent with existing provider icon patterns
    - Proper SVG accessibility with title attribute
    - Official Dropbox brand color (#0061FF)

---
+++ apps/web/src/components/icons/dropbox-icon.tsx
@@ -0,0 +1,12 @@
+export default function DropboxIcon() {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" role="img">
+			<title>Dropbox</title>
+			{/* Official Dropbox logo structure */}
+			<path
+				d="M7.004 3l-4.5 2.85 4.5 2.85 4.5-2.85L7.004 3zm9.992 0L12.5 5.85l4.496 2.85 4.5-2.85L16.996 3zM2.504 8.7l4.5 2.85 4.5-2.85-4.5-2.85L2.504 8.7zm14.992 0l-4.5 2.85 4.5 2.85 4.5-2.85-4.5-2.85zM7.004 14.4l-4.5 2.85L7.004 21l4.5-2.85-4.5-2.75zm9.992 0l-4.5 2.75L16.996 21l4.5-3.75-4.5-2.85z"
+				fill="#0061FF"
+			/>
+		</svg>
+	);
+}
